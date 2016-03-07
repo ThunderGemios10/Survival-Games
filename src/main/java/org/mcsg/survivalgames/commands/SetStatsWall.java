d02e2a895cb6b5c7f40f6860f1455fdecc803a8d
Romarjosh Villamor <marilyndris@gmail.com>
2016-03-07 03:37:59 -0800

    Re-Implement StatsWallManager

--- src/main/java/org/mcsg/survivalgames/commands/SetStatsWall.java
+++ src/main/java/org/mcsg/survivalgames/commands/SetStatsWall.java
@@ -2,13 +2,14 @@ package org.mcsg.survivalgames.commands;
 
 import org.bukkit.entity.Player;
 import org.mcsg.survivalgames.SettingsManager;
+import org.mcsg.survivalgames.stats.StatsWallManager;
 
 
 
 public class SetStatsWall implements SubCommand {
 
     public boolean onCommand(Player player, String[] args) {
-        //StatsWallManager.getInstance().setStatsSignsFromSelection(player);
+        StatsWallManager.getInstance().setStatsSignsFromSelection(player);
         return false;
     }
 

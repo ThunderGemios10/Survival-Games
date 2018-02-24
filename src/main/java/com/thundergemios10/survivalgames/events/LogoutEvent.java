8127651fd6be472f2d256a4e82781edcbbc1112e
Romarjosh Villamor <thundergemios10@gmail.com>
2018-02-24 16:25:30 +0800

    Fixes issue #59

--- src/main/java/com/thundergemios10/survivalgames/events/LogoutEvent.java
+++ src/main/java/com/thundergemios10/survivalgames/events/LogoutEvent.java
@@ -9,18 +9,18 @@ import com.thundergemios10.survivalgames.GameManager;
 
 
 
-public class LogoutEvent implements Listener{
+public class LogoutEvent implements Listener {
 
     
     @EventHandler
-    public void PlayerLoggout(PlayerQuitEvent e){
+    public void PlayerLogout(PlayerQuitEvent e) {
         Player p = e.getPlayer();
         GameManager.getInstance().removeFromOtherQueues(p, -1);
         int id = GameManager.getInstance().getPlayerGameId(p);
         if(GameManager.getInstance().isSpectator(p))
         	GameManager.getInstance().removeSpectator(p);
         if(id == -1) return;
-        if(GameManager.getInstance().getGameMode(id)==Game.GameMode.INGAME)
+        if(GameManager.getInstance().getGameMode(id)==Game.GameMode.INGAME || GameManager.getInstance().getGameMode(id)==Game.GameMode.STARTING)
             GameManager.getInstance().getGame(id).killPlayer(p, true);
         else
             GameManager.getInstance().getGame(id).removePlayer(p, true);

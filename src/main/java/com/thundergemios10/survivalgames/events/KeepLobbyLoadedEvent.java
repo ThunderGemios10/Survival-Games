8c25290876874a6b102e5731756ca9c047746295
remyboy2003 <remy.keil@hotmail.com>
2019-05-28 21:10:16 +0200

    Updated KeepLobbyLoadedEvent

--- src/main/java/com/thundergemios10/survivalgames/events/KeepLobbyLoadedEvent.java
+++ src/main/java/com/thundergemios10/survivalgames/events/KeepLobbyLoadedEvent.java
@@ -4,6 +4,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkUnloadEvent;
 import com.thundergemios10.survivalgames.LobbyManager;
+import com.thundergemios10.survivalgames.SurvivalGames;
 
 
 
@@ -15,7 +16,7 @@ public class KeepLobbyLoadedEvent implements Listener{
 		if(LobbyManager.lobbychunks.contains(e.getChunk())){
 //			e.setCancelled(true)
 //			TODO find a alternative way to keep the lobby chunks loaded
-			System.out.println("Lobby Chunk unloading");
+			SurvivalGames.debug("[KeepLobbyLoadedEvent] Lobby Chunk unloading");
         }
         //System.out.println("Chunk unloading");
     }
